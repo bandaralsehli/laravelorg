08fd9bfe66d74f6d8b815c29b3584c74e951d19e
aljorypro <48801153+bandaralsehli@users.noreply.github.com>
2022-10-25 22:43:56 +0300

    Update welcome.blade.php
    UPDATA tailwindcss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        
+        <script src="https://cdn.tailwindcss.com"></script>
 
         <!-- Styles -->
         <style>
